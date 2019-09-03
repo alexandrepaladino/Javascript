90e8bd0cd618a18386daf1bffdd1a87d42b42d9b
alexandrepaladino <31327195+alexandrepaladino@users.noreply.github.com>
2019-09-02 22:51:06 -0300

    Add files via upload

---
+++ JavaScript Array/higher.js
@@ -0,0 +1,22 @@
+const { people } = require('./people');
+
+const equals = property => value => obj => obj[property] === value;
+const getProperties = properties => obj => properties.reduce((get, item) => {
+    get[item] = obj[item];
+    return get;
+}, {});
+
+const older = property =>  (bigger, obj) =>  bigger = 
+        (bigger.hasOwnProperty(property)) ? 
+        (parseInt(obj[property]) > parseInt(bigger[property])) ? 
+        obj : bigger : obj;
+
+const getNameAge = getProperties(['name', 'age']);
+const teamCorinthians = equals('team')('Corinthians');
+
+const person = people
+            .filter(teamCorinthians)
+            .map(getNameAge) 
+            .reduce(older('age'), {});
+
+console.log(person);
